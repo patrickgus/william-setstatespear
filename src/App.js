9a27383d719e8ff77cc57c2d161a76196373acbb
Patrick Gustafson <patrickgus@charter.net>
2019-12-07 11:12:33 -0800

    add Route for PoemListPage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import PoemListPage from './PoemListPage'
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import PoemListPage from "./PoemListPage";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <nav>
-          <a href='/'>Poem List</a>
+          <a href="/">Poem List</a>
         </nav>
         <header>
           <h1>William Setstatespear</h1>
         </header>
         <main>
-          <PoemListPage />
+          <Route 
+            exact path='/'
+            component={PoemListPage}
+          />
         </main>
         <footer>
           <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
